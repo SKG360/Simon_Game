218b57aadcaa9e3d4c43069687ee5d6f27c2d392
SKG360 <samuel.giles@colorado.edu>
2020-04-16 16:03:21 -0600

    Adds sound to one button

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
 function nextSequence() {
   var buttonColors = ["red", "blue", "green", "yellow"];
   var gamePattern = [];
-  var randomNumber = Math.floor(Math.random() * Math.floor(4));
+  var randomNumber = Math.floor(Math.random() * 3);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
-  $("#" + randomChosenColor).click(function() {
+  $(document).on("click", function() {
   $("#" + randomChosenColor).fadeOut(50).fadeIn(50);
-});
+  var sound_play = new Audio('sounds/' + randomChosenColor + '.mp3');
+  sound_play.play();
+  });
 }
 console.log(nextSequence())
