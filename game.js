365ee1c45be4b813590b94febb93a9bbcf3a6c71
SKG360 <samuel.giles@colorado.edu>
2020-04-16 15:07:12 -0600

    adds random number generator

--- game.js
+++ game.js
@@ -4,5 +4,9 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * Math.floor(4));
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).click(function() {
+  $("#" + randomChosenColor).fadeOut(50).fadeIn(50);
+});
 }
 console.log(nextSequence())
