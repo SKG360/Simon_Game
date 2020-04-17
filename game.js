f41c1277e9df251dd95db74cc1792a85ee228226
SKG360 <samuel.giles@colorado.edu>
2020-04-16 22:16:50 -0600

    iteration 3

--- game.js
+++ game.js
@@ -1,14 +1,21 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
 function nextSequence() {
-  var buttonColors = ["red", "blue", "green", "yellow"];
-  var gamePattern = [];
-  var randomNumber = Math.floor(Math.random() * 3);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
-  $(document).on("click", function() {
-  $("#" + randomChosenColor).fadeOut(50).fadeIn(50);
-  var sound_play = new Audio('sounds/' + randomChosenColor + '.mp3');
-  sound_play.play();
+  $(document).on("keydown", function() {
+    $("#" + randomChosenColor).fadeOut(50).fadeIn(50);
+    var sound_play = new Audio('sounds/' + randomChosenColor + '.mp3');
+    sound_play.play();
+  });
+
+  $(".btn").on("click", function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
   });
 }
 console.log(nextSequence())
